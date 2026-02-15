6ec3eb529a4982128d6ef9c60a9533787cdea885
dating-site <ridsory@gmail.com>
2026-02-15 13:29:57 +0300

    Update keyboards.py

--- services/keyboards.py
+++ services/keyboards.py
@@ -3,7 +3,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 main_kb = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="📸 Создать скриншот", callback_data="screen")],
     [InlineKeyboardButton(text="💰 Баланс", callback_data="balance")],
-    [InlineKeyboardButton(text="✅ Успешный вывод", callback_data="success")]
+    [InlineKeyboardButton(text="✅ Успешный вывод", callback_data="success")],
+    [InlineKeyboardButton(text="📱 Скрин ТГ", callback_data="tg")]  # ← ДОБАВИЛИ
 ])
 
 back_kb = InlineKeyboardMarkup(inline_keyboard=[
